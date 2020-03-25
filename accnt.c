0d56d5f9b3661ef878ed52c01c344a19151e0cf9
JavaSE77 <35548843+JavaSE77@users.noreply.github.com>
2020-03-25 00:22:52 -0400

    Added methods to get RUSAGE struct and printf it

--- accnt.c
+++ accnt.c
@@ -1,2 +1,43 @@
 //This file is for the accounting information
 #include "accnt.h"
+#include <stdio.h>
+#include <stdlib.h> 
+#include <regex.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+int getAccnt(int who, struct rusage *usage) {
+    //Fill the pointer
+    if(getrusage(who, usage) > 0) {
+        return 1; 
+    } else {
+        fprintf(stderr, "Error getting rusage.\n");
+        return -1;
+    }
+
+}
+
+void printRusage(struct rusage *usage) {
+    printf("User CPU time used: %1.5lfs\n", (double) usage->ru_utime.tv_sec + ((double) usage->ru_utime.tv_usec / (double) 1000000));
+    printf("System CPU time used: %1.5lfs\n", (double) usage->ru_stime.tv_sec + ((double) usage->ru_stime.tv_usec / (double) 1000000));
+    printf("Max resident set size: %ld\n", usage->ru_maxrss);
+    printf("Integral shared memory size: %ld\n", usage->ru_ixrss);
+    printf("Integral unshared data size: %ld\n", usage->ru_idrss);
+    printf("Integral unshared stack size: %ld\n", usage->ru_isrss);
+    printf("Soft page faults: %ld\n", usage->ru_minflt);
+    printf("Hard page faults: %ld\n", usage->ru_majflt);
+    printf("Swaps: %ld\n", usage->ru_nswap);
+    printf("Block input operations: %ld\n", usage->ru_inblock);
+    printf("Block output operations: %ld\n", usage->ru_oublock);
+    printf("IPC messages: %ld\n", usage->ru_msgsnd);
+    printf("IPC messages received: %ld\n", usage->ru_msgrcv);
+    printf("Signals received: %ld\n", usage->ru_nsignals);
+    printf("Voluntary context switches: %ld\n", usage->ru_nvcsw);
+    printf("Involuntary context switches: %ld\n", usage->ru_nivcsw);
+}
